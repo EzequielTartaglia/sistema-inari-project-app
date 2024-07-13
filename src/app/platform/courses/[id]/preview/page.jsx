ba543e1f8078f6a853d58314cc05500156987f3c
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2024-07-13 19:33:59 -0300

    'Double-commit'

--- src/app/platform/courses/[id]/preview/page.jsx
+++
@@ -1,8 +0,0 @@
-import CoursePreviewPage from "@/src/views/Platform/CoursesPage/Course[id]/Preview/CoursePreviewPage";
-
-
-export default function CoursePreview({params}) {
-  return (
-    <CoursePreviewPage courseId={params.id}/>
-  )
-}

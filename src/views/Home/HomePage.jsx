108dbc73b2094a11ffe4d63c268340f7d1469e8a
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2024-07-18 14:16:45 -0300

    'Double-commit'

--- src/views/Home/HomePage.jsx
+++ src/views/Home/HomePage.jsx
@@ -9,7 +9,7 @@ export default function HomePage() {
       <div className="flex flex-col items-center md:items-end w-full sm:min-w-[640px] sm:max-w-[768px]   md:min-w-[768px] md:max-w-[1024px] lg:min-w-[1024px] lg:max-w-[1280px] xl:min-w-[1280px] xl:max-w-[1536px]">
               <Button
                 route={"/platform"}
-                customClasses="px-4 py-2 bg-primary text-title-active-static rounded-md shadow-md hover:bg-secondary transition duration-300 bg-primary border-secondary-light text-title-active-static font-semibold"
+                customClasses="px-4 py-2 bg-primary text-title-active-static rounded-md shadow-md hover:bg-secondary transition duration-300 bg-primary border-secondary-light text-title-active-static font-semibold gradient-button"
                 text={"Ingresar a la plataforma"}
                 title={"Ingresar a la plataforma"}
               />

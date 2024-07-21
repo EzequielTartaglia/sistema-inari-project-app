f9b54c7e864fca3c6e56a3b8e77a56d2505a516c
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2024-07-21 20:14:09 -0300

    'Double-commit'

---
+++ src/models/platform/product/product.js
@@ -0,0 +1,90 @@
+import supabase from "@/utils/supabase/supabaseClient";
+
+export async function getProducts() {
+  try {
+    const { data, error } = await supabase
+      .from("products")
+      .select("*");
+    if (error) {
+      throw error;
+    }
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function addProduct(name, description, imagePath, product_category_id, price, product_measure_unit_id, quantity) {
+  try {
+    const { data, error } = await supabase.from("products").insert({
+      name: name,
+      description: description,
+      image_path: imagePath,
+      product_category_id: product_category_id,
+      price: price,
+      product_measure_unit_id: product_measure_unit_id,
+      quantity: quantity
+    });
+    if (error) {
+      throw error;
+    }
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function getProduct(product_id) {
+  try {
+    const { data, error } = await supabase
+      .from("products")
+      .select("*")
+      .eq("id", product_id)
+      .single();
+    if (error) {
+      throw error;
+    }
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function editProduct(product_id, name, description, imagePath, product_category_id, price, product_measure_unit_id, quantity) {
+  try {
+    const { data, error } = await supabase
+      .from("products")
+      .update({
+        name: name,
+        description: description,
+        image_path: imagePath,
+        product_category_id: product_category_id,
+        price: price,
+        product_measure_unit_id: product_measure_unit_id,
+        quantity: quantity
+      })
+      .eq("id", product_id);
+    if (error) {
+      throw error;
+    }
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function deleteProduct(product_id) {
+  try {
+    const { data, error } = await supabase
+      .from("products")
+      .delete()
+      .eq("id", product_id)
+      .single();
+    if (error) {
+      throw error;
+    }
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}

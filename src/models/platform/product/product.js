5413c6ad1f856f5aedfbc70230029a10cd878ce1
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2024-07-21 20:14:36 -0300

    'Double-commit'

--- src/models/platform/product/product.js
+++ src/models/platform/product/product.js
@@ -14,12 +14,12 @@ export async function getProducts() {
   }
 }
 
-export async function addProduct(name, description, imagePath, product_category_id, price, product_measure_unit_id, quantity) {
+export async function addProduct(name, description, image_path, product_category_id, price, product_measure_unit_id, quantity) {
   try {
     const { data, error } = await supabase.from("products").insert({
       name: name,
       description: description,
-      image_path: imagePath,
+      image_path: image_path,
       product_category_id: product_category_id,
       price: price,
       product_measure_unit_id: product_measure_unit_id,
@@ -50,14 +50,14 @@ export async function getProduct(product_id) {
   }
 }
 
-export async function editProduct(product_id, name, description, imagePath, product_category_id, price, product_measure_unit_id, quantity) {
+export async function editProduct(product_id, name, description, image_path, product_category_id, price, product_measure_unit_id, quantity) {
   try {
     const { data, error } = await supabase
       .from("products")
       .update({
         name: name,
         description: description,
-        image_path: imagePath,
+        image_path: image_path,
         product_category_id: product_category_id,
         price: price,
         product_measure_unit_id: product_measure_unit_id,

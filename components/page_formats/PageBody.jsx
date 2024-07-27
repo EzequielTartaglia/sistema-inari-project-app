ad2bb0944e1b0cc0b90540cde920b0cd4a0fbee5
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2024-07-27 01:35:55 -0300

    'Double-commit'

--- components/page_formats/PageBody.jsx
+++ components/page_formats/PageBody.jsx
@@ -4,9 +4,9 @@ export default function PageBody({ children }) {
       <div
         className="flex flex-col items-center p-2 w-full
         sm:min-w-[640px] sm:max-w-[768px]
-        md:min-w-[769px] md:max-w-[1024px]
+        md:min-w-[768px] md:max-w-[1024px]
         lg:min-w-[930px] w-[768px] lg:max-w-[768px]
-        xl:min-w-[1281px] w-[1280px] xl:max-w-[1536px]"
+        xl:min-w-[1280px] w-[1280px] xl:max-w-[1536px]"
       >
         {children}
       </div>

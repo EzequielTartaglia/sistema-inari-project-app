a7d26190965327c9201495dd51ebc9976948f6b3
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2024-07-28 23:22:52 -0300

    'Double-commit'

--- src/app/platform/platform_settings/page.jsx
+++ src/app/platform/platform_settings/page.jsx
@@ -1,7 +1,6 @@
 import ConditionalSessionRender from "@/src/helpers/ConditionalSessionRender";
 import NotPermissionPage from "@/src/views/Platform/NotPermissionPage/NotPermissionPage";
 import PlatformSettingsPage from "@/src/views/Platform/PlatformSettings/PlatformSettingsPage";
-import ProductCategoriesPage from "@/src/views/Platform/ProductCategories/ProductCategoriesPage";
 
 export default function PlatformSettings() {
   return (

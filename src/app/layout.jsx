a2e432eb80996e35c952852412266ce145a59cf5
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2024-07-30 12:52:31 -0300

    'Double-commit'

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -34,7 +34,6 @@ export default function RootLayout({ children }) {
                   <PageBody>{children}</PageBody>
                 </main>
                 <FooterWrapper />
-                <Credits />
               </AsideNavBarProvider>
             </UserInfoProvider>
           </NotificationProvider>

4649263ffcaaedad4dceed79c078831fdecc938a
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2024-08-08 21:23:33 -0300

    'Double-commit'

--- components/footers/Credits.jsx
+++ components/footers/Credits.jsx
@@ -2,9 +2,8 @@
 
 import Link from "next/link";
 
-export default function Credits() {
-
-  const systemName = process.env.NEXT_PUBLIC_SYSTEM_NAME
+export default function Credits({paddingClass = 'p-1'}) {
+  const systemName = process.env.NEXT_PUBLIC_SYSTEM_NAME;
 
   const developerInfo = {
     name: "Ezequiel",
@@ -15,20 +14,35 @@ export default function Credits() {
     github: "https://github.com/ezequieltartaglia",
   };
 
-  const middleInitial = developerInfo.middleName ? developerInfo.middleName.charAt(0) : '';
+  const middleInitial = developerInfo.middleName ? developerInfo.middleName.charAt(0) : "";
 
   const currentYear = new Date().getFullYear();
 
   return (
-      <div className="bg-secondary text-primary p-1 text-center font-semibold">
-        {systemName}&#174; | Desarrollado por 
+    <div className={`bg-secondary text-primary ${paddingClass} text-center font-semibold`}>
+      <div className="hidden sm:block">
+        {systemName}&#174; | Desarrollado por
         <Link
           href={developerInfo.linkedin}
           className="text-title-active inline-flex items-center justify-center mx-1"
         >
-           {developerInfo.name} {middleInitial}. {developerInfo.surname}
+          {developerInfo.name} {middleInitial}. {developerInfo.surname}
         </Link>
         | Todos los derechos reservados {currentYear}&copy;
       </div>
+      <div className="block sm:hidden">
+        <div>{systemName}&#174;</div>
+        <div>
+          Desarrollado por
+          <Link
+            href={developerInfo.linkedin}
+            className="text-title-active inline-flex items-center justify-center mx-1"
+          >
+            {developerInfo.name} {middleInitial}. {developerInfo.surname}
+          </Link>
+        </div>
+        <div>Todos los derechos reservados {currentYear}&copy;</div>
+      </div>
+    </div>
   );
 }
--- components/footers/platform/PlatformFooter.jsx
+++ components/footers/platform/PlatformFooter.jsx
@@ -1,37 +1,9 @@
-import Button from "@/components/Button";
-import Logo from "@/components/Logo";
-import SocialMedia from "@/components/SocialMedia";
 import Credits from "../Credits";
-import { FiFacebook, FiInstagram, FiX, FiYoutube } from "react-icons/fi";
 
-
-export default function PlatformFooter({ items }) {
+export default function PlatformFooter() {
     return (
       <div className="ml-[50px]">
-        <footer className="footer-bg-primary-main text-primary p-4 pt-0 font-semibold">
-          <div className="grid grid-cols-1 lg:grid-cols-2 justify-center gap-4 max-w-[1140px] mx-auto py-[25px] px-5 gap-6">
-            <div className="lg:col-span-1">
-              <div className="logo-container" style={{ textAlign: "-webkit-center" }}>
-                <Logo isFooter={true} />
-                <div className="border-b footer-border mt-4 w-[300px]"></div>
-              </div>
-              <div className="flex justify-center mt-2">
-                <SocialMedia route="https://www.instagram.com/" icon={<FiInstagram size={24}/>} alt="Instagram" className="mx-2" />
-                <SocialMedia route="https://x.com/" icon={<FiX size={24}/>} alt="x" className="mx-2" />
-                <SocialMedia route="https://www.youtube.com/" icon={<FiYoutube size={24}/>} alt="YouTube" className="mx-2" />
-                <SocialMedia route="https://www.facebook.com/" icon={<FiFacebook size={24}/>} alt="Facebook" className=" mx-2" />
-              </div>
-            </div>
-            <div className="lg:col-span-1 flex flex-col gap-1 text-center lg:text-center">
-              {items.map((item, index) => (
-                <div key={index} className="text-primary p-1 text-center lg:text-center">
-                  <Button customClasses="text-title inline-flex items-center justify-center shadow-none" route={item.route} text={item.text} title={item.text}/>
-                </div>
-              ))}
-            </div>
-          </div>
-        </footer>
-        <Credits/>
+        <Credits paddingClass="p-4"/>
       </div>
     );
 }

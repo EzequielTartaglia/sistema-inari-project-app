f889e92478c5195e165a46b764c328e97a5b614d
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2024-08-24 13:08:46 -0300

    'Double-commit'

--- components/forms/SubmitLoadingButton.jsx
+++ components/forms/SubmitLoadingButton.jsx
@@ -2,7 +2,7 @@ const SubmitLoadingButton = ({ isLoading, children, submitText = "Procesando..."
   return (
     <button
       {...props}
-      className={`mt-5 border-2 text-center h-[44px] px-8 rounded-[5px] font-semibold ${isLoading ? 'bg-disabled border-disabled' : 'secondary-bg-button hover:secondary-bg-button hover:border-opacity-10 border-primary'} transition duration-300 ease-in-out w-full text-primary hover:border-opacity-10 ${isLoading ? 'cursor-not-allowed' : ''}`}
+      className={`mt-5 border-2 text-center text-sm md:text-lg h-[44px] px-8 rounded-[5px] font-semibold ${isLoading ? 'button-disabled' : 'secondary-bg-button hover:secondary-bg-button hover:border-opacity-10 border-primary'} transition duration-300 ease-in-out w-full text-primary hover:border-opacity-10 ${isLoading ? 'cursor-not-allowed' : ''}`}
       disabled={isLoading}
     >
       {isLoading ? submitText : children}

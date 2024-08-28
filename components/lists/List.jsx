265b94e32346a06becf16def1751b4e3798816f6
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2024-08-28 15:36:44 -0300

    'Double-commit'

--- components/lists/List.jsx
+++ components/lists/List.jsx
@@ -1,5 +1,16 @@
 import { useState } from "react";
-import { FiEye, FiEdit, FiTrash2, FiPlus, FiSettings } from "react-icons/fi";
+import {
+  FiEye,
+  FiEdit,
+  FiTrash2,
+  FiPlus,
+  FiSettings,
+  FiFileText,
+  FiChevronRight,
+  FiChevronLeft,
+  FiChevronsRight,
+  FiChevronsLeft,
+} from "react-icons/fi";
 import ConfirmModal from "../ConfirmModal";
 import Link from "next/link";
 import formatDate from "@/src/helpers/formatDate";
@@ -18,20 +29,26 @@ export default function List({
   columnName3,
   confirmModalText,
   hasShow = (id) => false,
-  hasShowIcon = <FiEye className="text-show-link" size={24}/>,
+  hasShowIcon = <FiEye className="text-show-link" size={24} />,
   hasEdit = true,
   hasDelete = true,
   hasExtraButton = () => false,
-  extraButtonIcon = <FiSettings className="text-title-active" size={24}/>,
+  extraButtonIcon = <FiSettings className="text-title-active" size={24} />,
   extraButtonTitle = () => "",
   onExtraButtonClick,
   hasExtraButton2 = () => false,
-  extraButtonIcon2 = <FiSettings className="text-title-active" size={24}/>,
+  extraButtonIcon2 = <FiSettings className="text-title-active" size={24} />,
   extraButtonTitle2 = () => "",
   onExtraButtonClick2,
+  hasExtraButton3 = () => false,
+  extraButtonIcon3 = <FiFileText className="text-title-active" size={24} />,
+  extraButtonTitle3 = () => "",
+  buttonEditRoute3 = null,
+  itemsPerPage = 15,
 }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [currentId, setCurrentId] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
 
   const handleDelete = (id) => {
     setCurrentId(id);
@@ -57,6 +74,20 @@ export default function List({
     return aValue.localeCompare(bValue);
   });
 
+  // Pagination logic
+  const totalPages = Math.ceil(sortedList.length / itemsPerPage);
+  const currentPageItems = sortedList.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
+
+  const handleFirstPage = () => setCurrentPage(1);
+  const handlePreviousPage = () =>
+    setCurrentPage((prev) => Math.max(prev - 1, 1));
+  const handleNextPage = () =>
+    setCurrentPage((prev) => Math.min(prev + 1, totalPages));
+  const handleLastPage = () => setCurrentPage(totalPages);
+
   return (
     <>
       <div className="relative">
@@ -71,14 +102,18 @@ export default function List({
           </Link>
         )}
         <ul className="flex flex-col gap-5 text-primary list-none p-0 w-full">
-          {sortedList.map((item) => (
+          {currentPageItems.map((item) => (
             <li
               key={item.id}
               className="bg-primary rounded-md px-6 py-3 shadow-md transition duration-300 hover:-translate-y-1 bg-secondary w-full border-primary relative flex items-center justify-between"
             >
               <div className="flex items-center space-x-2 overflow-hidden truncate sm:truncate max-w-[400px]">
                 <div className="pr-16 sm:pr-16 md:pr-0 lg:pr-0">
-                  <span>{columnNameIsDate ? formatDate(item[columnName]) : item[columnName]}</span>{" "}
+                  <span>
+                    {columnNameIsDate
+                      ? formatDate(item[columnName])
+                      : item[columnName]}
+                  </span>{" "}
                   {labelColumnName2 && (
                     <span className="ml-3 mr-3">
                       {labelColumnName2}: {item[columnName2]}
@@ -91,7 +126,17 @@ export default function List({
                   )}
                 </div>
               </div>
+
               <div className="flex items-center space-x-2">
+                {hasExtraButton3(item.id) && (
+                  <Link
+                    href={buttonEditRoute3(item.id)}
+                    className="ml-2 flex-shrink-0"
+                    title="Mas informacion"
+                  >
+                    {extraButtonIcon3}
+                  </Link>
+                )}
                 {hasExtraButton(item.id) && (
                   <button
                     className="ml-2"
@@ -125,7 +170,7 @@ export default function List({
                     className="ml-2 flex-shrink-0"
                     title="Editar"
                   >
-                    <FiEdit className="text-edit-link" size={24}/>
+                    <FiEdit className="text-edit-link" size={24} />
                   </Link>
                 )}
                 {hasDelete && (
@@ -134,13 +179,53 @@ export default function List({
                     className="ml-2 flex-shrink-0"
                     title="Eliminar"
                   >
-                    <FiTrash2 className="text-delete-link" size={24}/>
+                    <FiTrash2 className="text-delete-link" size={24} />
                   </button>
                 )}
               </div>
             </li>
           ))}
         </ul>
+
+        {/* Pagination Controls */}
+        {totalPages > 1 && (
+          <div className="flex flex-col items-center mt-4">
+            <div className="flex justify-center">
+              <button
+                onClick={handleFirstPage}
+                disabled={currentPage === 1}
+                className="p-1 mx-1 bg-secondary text-primary rounded hover:bg-primary hover:text-secondary disabled:opacity-50"
+              >
+                <FiChevronsLeft size={24} />
+              </button>
+              <button
+                onClick={handlePreviousPage}
+                disabled={currentPage === 1}
+                className="p-1 mx-1 bg-secondary text-primary rounded hover:bg-primary hover:text-secondary disabled:opacity-50"
+              >
+                <FiChevronLeft size={24} />
+              </button>
+              <button
+                onClick={handleNextPage}
+                disabled={currentPage === totalPages}
+                className="p-1 mx-1 bg-secondary text-primary rounded hover:bg-primary hover:text-secondary disabled:opacity-50"
+              >
+                <FiChevronRight size={24} />
+              </button>
+              <button
+                onClick={handleLastPage}
+                disabled={currentPage === totalPages}
+                className="p-1  mx-1 bg-secondary text-primary rounded hover:bg-primary hover:text-secondary disabled:opacity-50"
+              >
+                <FiChevronsRight size={24} />
+              </button>
+            </div>
+            {/* Display current page and total pages */}
+            <p className="text-primary mt-2">
+              Página {currentPage} / {totalPages}
+            </p>
+          </div>
+        )}
       </div>
 
       <ConfirmModal

0a9b8371388a96876ab196d6d3bb6df54456ddaf
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2024-09-19 22:27:15 -0300

    'Double-commit'

---
+++ src/models/platform/sale/sale.js
@@ -0,0 +1,90 @@
+import supabase from "@/utils/supabase/supabaseClient";
+
+export async function getSales() {
+  try {
+    const { data, error } = await supabase.from("sales").select("*");
+    if (error) {
+      throw error;
+    }
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function addSale(user_id, sale_date, sale_total, is_closed) {
+  try {
+    const { data, error } = await supabase
+      .from("sales")
+      .insert({
+        user_id: user_id,
+        sale_date: sale_date,
+        sale_total: sale_total,
+        is_closed: is_closed,
+      });
+    if (error) {
+      throw error;
+    }
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function getSale(sale_id) {
+  try {
+    const { data, error } = await supabase
+      .from("sales")
+      .select("*")
+      .eq("id", sale_id)
+      .single();
+    if (error) {
+      throw error;
+    }
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function editSale(
+  sale_id,
+  user_id,
+  sale_date,
+  sale_total,
+  is_closed
+) {
+  try {
+    const { data, error } = await supabase
+      .from("sales")
+      .update({
+        user_id: user_id,
+        sale_date: sale_date,
+        sale_total: sale_total,
+        is_closed: is_closed,
+      })
+      .eq("id", sale_id);
+    if (error) {
+      throw error;
+    }
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function deleteSale(sale_id) {
+  try {
+    const { data, error } = await supabase
+      .from("sales")
+      .delete()
+      .eq("id", sale_id)
+      .single();
+    if (error) {
+      throw error;
+    }
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
---
+++ src/models/platform/sale_item/sale_item.js
@@ -0,0 +1,93 @@
+import supabase from "@/utils/supabase/supabaseClient";
+
+export async function getSaleItems() {
+  try {
+    const { data, error } = await supabase.from("sale_items").select("*");
+    if (error) {
+      throw error;
+    }
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function addSaleItem(
+  sale_id,
+  product_id,
+  quantity = 1,
+  sale_item_total
+) {
+  try {
+    const { data, error } = await supabase.from("sale_items").insert({
+      sale_id: sale_id,
+      product_id: product_id,
+      quantity: quantity,
+      sale_item_total: sale_item_total,
+    });
+    if (error) {
+      throw error;
+    }
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function getSaleItem(sale_item_id) {
+  try {
+    const { data, error } = await supabase
+      .from("sale_items")
+      .select("*")
+      .eq("id", sale_item_id)
+      .single();
+    if (error) {
+      throw error;
+    }
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function editSaleItem(
+  sale_item_id,
+  sale_id,
+  product_id,
+  quantity,
+  sale_item_total
+) {
+  try {
+    const { data, error } = await supabase
+      .from("sale_items")
+      .update({
+        sale_id: sale_id,
+        product_id: product_id,
+        quantity: quantity,
+        sale_item_total: sale_item_total,
+      })
+      .eq("id", sale_item_id);
+    if (error) {
+      throw error;
+    }
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function deleteSaleItem(sale_item_id) {
+  try {
+    const { data, error } = await supabase
+      .from("sale_items")
+      .delete()
+      .eq("id", sale_item_id)
+      .single();
+    if (error) {
+      throw error;
+    }
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}

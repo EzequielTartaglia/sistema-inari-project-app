9f9d3a9265199cf752b5b68b90cf4d09b74d1109
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2025-03-03 01:34:39 -0300

    'Double-commit'

--- src/app/platform/sales/page.jsx
+++ src/app/platform/sales/page.jsx
@@ -5,8 +5,8 @@ import SalesPage from "@/src/views/Platform/Sales/SalesPage";
 export default function Sales() {
   return (
     <ConditionalSessionRender
-    AuthorizedUserRoles={[1, 2, 3, 4, 6, 7]}
-    enablePluginsRequireds={[2]}
+      AuthorizedUserRoles={[1, 2, 3, 4, 6, 7]}
+      enablePluginsRequireds={[2]}
       ComponentIfUser={<SalesPage />}
       ComponentIfNoUser={<NotPermissionPage />}
     />
